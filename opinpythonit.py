456a154e7158c85b8b62eb4fd158f9796d17ec75
M2rtenRe <35971168+M2rtenRe@users.noreply.github.com>
2018-01-30 20:53:15 +0200

    Update opinpythonit.py

--- opinpythonit.py
+++ opinpythonit.py
@@ -1,27 +1,30 @@
+#Parooli muukija, mida ei tohiks kasutada kurjaks eesmärgiks ;)
+#Programm muugib 1,000,000 parooli 5 sekundiga
+
 import hashlib
 import threading
 from queue import Queue
 import time
-
+#Kasutaja input hashi ja parooli faili jaoks
 passIn = input("Hash: ")
 pwFile = input("Parooli fail: ")
-
+#Proovib leida parooli faili
 try:
     pwFile = open(pwFile, "r")
 except:
     print("\n Faili ei leitud")
     quit()
-
+#Peamine muukimisfunktsioon
 def passCrack():
     for password in pwFile:
-        filehash = hashlib.md5(password.strip().encode()).hexdigest()
-
+        filehash = hashlib.md5(password.strip().encode()).hexdigest() #Teeb hashi igast paroolist mis on paroolide failis
+        #Kui parooli failist saadud hash on sama mis algul antud hash, siis programm annab sõnumi ja lõpeb
         if passIn == filehash:
             print("\n Parool leitud.\n Parool on {}".format(password))
             break
-    else:
+    else:   #Kui ei leia, siis fail lõpeb
         pass
-
+#Threadimis funktsioon
 def threader():
     while True:
         worker = q.get()
@@ -29,14 +32,14 @@ def threader():
         q.task_done()
 
 q = Queue()
-
+#Kasutab 500 threadi
 for i in range(500):
     t = threading.Thread(target=threader)
     t.daemon = True
     t.start()
 
 startTime = time.time()
-
+#Iga thread teeb 10,000 parooli hashiks ja kui tehtud, siis alustab otsast peale
 for worker in range(1,10000):
     q.put(worker)
 
